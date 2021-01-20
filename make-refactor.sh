24020334a7cab9510b4064d29fbf9946a5efc527
Elias Bakken <elias.bakken@gmail.com>
2021-01-20 16:59:20 +0100

    Updated script with right filename (#147)
    Co-authored-by: Elias Bakken <elias@iagent.no>

--- make-refactor.sh
+++ make-refactor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 set -e
->/root/make-kamikaze.log
-exec >  >(tee -ia /root/make-kamikaze.log)
-exec 2> >(tee -ia /root/make-kamikaze.log >&2)
+>/root/make-refactor.log
+exec >  >(tee -ia /root/make-refactor.log)
+exec 2> >(tee -ia /root/make-refactor.log >&2)
 
 if [[ $# -ne 1 ]]; then
     echo "You need to specify the platform when you run the script."
@@ -11,6 +11,6 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-ansible-playbook system_klipper_octoprint-DEFAULT.yml --extra-vars "{\"platform\":\"$1\", \"packaging\":\"apt\"}"
+ansible-playbook SYSTEM_klipper_octoprint-DEFAULT.yml --extra-vars "{platform: $1}" -i hosts
 
 echo "Now reboot!"
