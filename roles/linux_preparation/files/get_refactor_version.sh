f5f598a6570ddfed751769604f57b2124aab861f
Jon C <goeland86@gmail.com>
2021-02-13 18:26:51 +0100

    Update the git tag check (#162)
    Fixing the git tag check that was returning the tag twice: as the tag name, then the full git ref tag (i.e. origin/tags/<tagname>).

--- roles/linux_preparation/files/get_refactor_version.sh
+++ roles/linux_preparation/files/get_refactor_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-TAG=$(git log -n1 --pretty="format:%d" | sed "s/, /\n/g" | grep tag: | sed "s/tag: \|)//g" | tr -d '\n')
+TAG=$(git log -n1 --pretty="format:%d" | sed "s/, /\n/g" | grep tag: | grep -v origin | sed "s/tag: \|)//g" | tr -d '\n')
 
 if [[ $TAG == "" ]]; then
 	TAG=$(git reflog --decorate -1 | awk -F' ' '{print $1}' | tr -d '\n')
