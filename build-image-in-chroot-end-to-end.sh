9c7e8e2edcd4534395368f33362f82f922c31500
Jon Charnas <goeland86@gmail.com>
2023-03-12 11:45:55 +0100

    Adding apt clean to free up disk space of cached downloads.

--- build-image-in-chroot-end-to-end.sh
+++ build-image-in-chroot-end-to-end.sh
@@ -111,7 +111,8 @@ chroot ${MOUNTPOINT} su -c "\
 cd ${REFACTOR_HOME} && \
 apt update && DEBIAN_FRONTEND=noninteractive apt -y upgrade && \
 DEBIAN_FRONTEND=noninteractive apt install -y ansible python3 python2 && \
-ansible-playbook ${SYSTEM_ANSIBLE} -T 180 --extra-vars '${ANSIBLE_PLATFORM_VARS}' -i hosts"
+ansible-playbook ${SYSTEM_ANSIBLE} -T 180 --extra-vars '${ANSIBLE_PLATFORM_VARS}' -i hosts && \
+apt clean"
 
 status=$?
 set -e
