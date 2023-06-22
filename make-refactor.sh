37bc50b832df8918a7a01cb2f6502e5f67f04eb9
Jon C <goeland86@gmail.com>
2023-06-22 11:55:44 +0200

    Cleaning up the roles of unnecessary elements

--- make-refactor.sh
+++ make-refactor.sh
@@ -7,10 +7,10 @@ exec 2> >(tee -ia /root/make-refactor.log >&2)
 
 if [[ $# -ne 1 ]]; then
     echo "You need to specify the platform when you run the script."
-    echo "It currently needs to be one of [recore, replicape, pi, pi64]"
+    echo "It currently needs to be recore"
     exit 1
 fi
 
-ansible-playbook SYSTEM_klipper_octoprint-DEFAULT.yml --extra-vars "{platform: $1}" -i hosts
+ansible-playbook SYSTEM_klipper_mainsail-DEFAULT.yml --extra-vars "{platform: $1}" -i hosts
 
 echo "Now reboot!"
