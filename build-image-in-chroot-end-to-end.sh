98069ad87c0562611b9a090fa0e0dbfe006478aa
Jon Charnas <goeland86@gmail.com>
2024-04-15 12:43:46 +0200

    Installing ansible as a pip package instead of via apt.

--- build-image-in-chroot-end-to-end.sh
+++ build-image-in-chroot-end-to-end.sh
@@ -118,10 +118,12 @@ update-locale LC_ALL=en_US.UTF-8 && \
 apt update && DEBIAN_FRONTEND=noninteractive apt -y upgrade << EOF
 y
 EOF && \
-apt install -y ansible build-essential << EOF
+apt install -y python3-pip build-essential << EOF
 y
 EOF"
 chroot ${MOUNTPOINT} su -c "\
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && \
+python3 get-pip.py && \
 LC_ALL=en_US.UTF-8 ansible-playbook ${SYSTEM_ANSIBLE} -T 180 --extra-vars '${ANSIBLE_PLATFORM_VARS}' -i hosts -e 'ansible_python_interpreter=/usr/bin/python3'"
 
 status=$?
